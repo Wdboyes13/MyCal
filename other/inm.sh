cd6db1a66fadcf9992634a3312921e18ee86963b
Wdboyes13 <willdev2025@outlook.com>
2025-06-11 21:33:00 -0700

    Updated at Wed Jun 11 21:32:59 PDT 2025

---
+++ other/inm.sh
@@ -0,0 +1,29 @@
+cd ~/.mycal
+mkdir mail
+cd mail
+
+git clone --depth 1 https://gitlab.com/muttmua/mutt.git
+cd mutt
+./prepare
+./configure \
+  --disable-warnings \
+  --enable-gpgme \
+  --enable-hcache \
+  --enable-imap \
+  --enable-pop \
+  --enable-sidebar \
+  --enable-smtp \
+  --with-gss \
+  --with-idn2 \
+  --with-sasl \
+  --with-ssl=$(brew --prefix openssl@3) \
+  --with-tokyocabinet \
+  --disable-debug \
+  --disable-dependency-tracking \
+  --prefix="$HOME/.mycal/mail" \
+  --disable-nls
+make
+sudo make install
+cd ..
+rm -rf mutt
+
---
+++ other/rnm.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+$HOME/.mycal/mail/bin/mutt -F $HOME/.mycal/mail/.muttrc
